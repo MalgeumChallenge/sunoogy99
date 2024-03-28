ec8a998de850603e692893e3b43a48ba4202e2eb
sunoogy99 <76517609+sunoogy99@users.noreply.github.com>
2024-03-28 21:06:54 +0900

    Add files via upload

---
+++ 1주차/[CTR]_9회.cpp
@@ -0,0 +1,45 @@
+// https://www.codetree.ai/missions/2/problems/maximin-path-in-square/description
+/*
+	생각한 로직
+	
+	위, 왼쪽 중 큰 값과 현재 내 값 비교했을 때, 값 비교하여 더 작은 수를 입력해준다.
+	min(max(dp[i - 1][j], dp[i][j - 1]), arr[i][j])
+*/
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+using namespace std;
+
+int n;
+int arr[101][101];
+int dp[101][101];
+
+bool InRange(int x, int y) {
+	return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+			dp[i][j] = arr[i][j];
+		}
+	}
+
+	int min_val = INT_MAX;
+	for (int i = 1; i < n; i++) {
+		dp[i][0] = min(dp[i - 1][0], arr[i][0]);
+		dp[0][i] = min(dp[0][i - 1], arr[0][i]);
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < n; j++) {
+			dp[i][j] = min(max(dp[i - 1][j], dp[i][j - 1]), arr[i][j]);
+		}
+	}
+
+	cout << dp[n - 1][n - 1];
+	return 0;
+}
